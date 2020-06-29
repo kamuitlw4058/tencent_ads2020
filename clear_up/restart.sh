f9534de917e6788798b81a9bfd6c772a508e39b7
IvanJobs <ivan-he@zamplus.com>
2020-06-29 10:17:28 +0800

    update

--- clear_up/restart.sh
+++ clear_up/restart.sh
@@ -1,5 +1,7 @@
 t=`cat last_cmd`
 killp $t
-echo $1 > last_cmd
 d=`date "+%Y%m%d%H%M%S"`
-nohup python -u  $1 tencent_ads >logs/$1_$d.log & 
+mv nohup.out logs/$t_$d.log
+
+echo $1 > last_cmd
+nohup python -u  $1 tencent_ads  & 
